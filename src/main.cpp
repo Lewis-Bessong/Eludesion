33d1a268643aa0caa01c7b02dfc2e09b7dfc0552
Lewis Bessong <lewistkbessong@gmail.com>
2026-08-05 17:30:12 -0500

    Looked at raylib example removed class, added ball position

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,15 @@
 const int WindowHeight = 800;
 const int WindowWidth = 800; 
 
-struct GameCircle { // class for circle (draw circle vector 2)
-
-    Vector2 center; 
-    float radius;
-    Color color;
-
-    int Width = 400;
-    int Height = 400;
-
-};
 
 int main()
 {
 
     InitWindow(WindowWidth, WindowHeight, "Lewis's first program");
     SetTargetFPS(60);
+
+    Vector2 BallPosition = { (float)WindowWidth/2, (float)WindowHeight/2}; // middle of screen
+
 
     // loop
     while (WindowShouldClose() == false)
@@ -31,20 +24,13 @@ int main()
 
             ClearBackground(WHITE);
     
-            GameCircle RedCircle = { {RedCircle.Width, RedCircle.Height}, 50, RED};
+            DrawCircleV(BallPosition, 50, RED);
 
             // Movement Logic 
 
             // Keyboard
 
-            if (IsKeyPressed(KEY_W)) {
-
-                RedCircle.Width += 3;
-            } if (IsKeyPressed(KEY_S)) {
-
-                RedCircle.Height += 3;
-            }
-            
+            if (IsKeyPressed(KEY_D)) BallPosition.x += 3.0f;
 
         }
         EndDrawing();
