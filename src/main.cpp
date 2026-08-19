ed0485a340d63ef947a38e474f7ea065b148606b
Lewis Bessong <lewistkbessong@gmail.com>
2026-08-18 19:36:14 -0500

    Removed Rectangle drawing to focus on window dimensions and trying to center drawings.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,15 @@
 #include <string>
 
 // Size of startup
-const int WindowWidth = 800; 
-const int WindowHeight = 800;
- 
+int WindowWidth = GetScreenWidth(); 
+int WindowHeight = GetScreenWidth();
 
 int main()
 {
     // ======= Window Section ======
 
     SetConfigFlags(FLAG_WINDOW_ALWAYS_RUN | FLAG_VSYNC_HINT); // Program can still run when minimized and can have a fps near users monitor refresh rate
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_MAXIMIZED); // Program can be resizeable and enter in maximized state
+    // SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_MAXIMIZED); // Program can be resizeable and enter in maximized state
     InitWindow(WindowWidth, WindowHeight, "Eludesion"); // program start 
     
     // window Icon
@@ -23,12 +22,9 @@ int main()
 
     // ======= Shapes/Drawing =======
 
+
     // Circle 
     Vector2 BallPosition = { (float)WindowWidth/2, (float)WindowHeight/2}; // middle of screen
-
-    // rectangle 
-    Vector2 RectanglePosition = { (float)WindowWidth/2, (float)WindowHeight/2};
-    Vector2 RectangleSize = { (float)200.0f, (float)200.0f};
     
     int gamepadIdx = 0; // for controller input 
 
@@ -40,14 +36,12 @@ int main()
         {                  
             // ====== Window Dimensions =====
             SetWindowState(FLAG_WINDOW_MAXIMIZED); // Program stays in maximized state (still minimizable) 
+            
            
             // ====== Objects Creation =======
-
             ClearBackground(WHITE);
     
             DrawCircleV(BallPosition, 50, RED);
-
-            DrawRectangleV(RectanglePosition, RectangleSize, BLACK);
 
             // ====== Movement Logic =======
 
@@ -62,7 +56,7 @@ int main()
             // Controller
             if (IsGamepadAvailable(gamepadIdx)) { // if controller connected
 
-                DrawText("Controller Connected.", WindowWidth/2, 0, 30, BLACK);
+                DrawText("Controller Connected", WindowWidth/2, 0, 30, BLACK);
 
                 if (IsGamepadButtonDown(gamepadIdx, GAMEPAD_BUTTON_LEFT_FACE_RIGHT)) { // right arrow
 
